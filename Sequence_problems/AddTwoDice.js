b7971102812700c7c462356bb561ed48bfd62a9e
kamurudivya <93344763+kamurudivya@users.noreply.github.com>
2022-01-20 00:18:32 +0530

    Added: Addition of two random dice number

---
+++ Sequence_problems/AddTwoDice.js
@@ -0,0 +1,9 @@
+/*
+* Addition of two random dice number
+*/
+var dice1 = Math.floor(Math.random() * 6) + 1;
+console.log("1st random number is:" + dice1);
+var dice2 = Math.floor(Math.random() * 6) + 1;
+console.log("2nd random number is:" + dice2);
+addition = dice1 + dice2;
+console.log("Addition of two random dicenumber:" + addition);

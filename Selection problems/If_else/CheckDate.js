f9ff2feb6d83807d721f14a47636598234380d28
kamurudivya <93344763+kamurudivya@users.noreply.github.com>
2022-01-20 00:41:53 +0530

    Added : To Check between date result and return Boolean value

---
+++ Selection problems/If_else/CheckDate.js
@@ -0,0 +1,9 @@
+/*
+* Check between date result and return boolean value 
+*/
+const prompts = require("prompt-sync")();
+let day = prompts("Enter day  : ");
+let month = prompts("Enter month : ");
+month.toLowerCase;
+if ((month == "march" || month == "april" || month == "may" || month == "june") && (day >= 20 & day <= 31)) console.log(true);
+else console.log(false);
